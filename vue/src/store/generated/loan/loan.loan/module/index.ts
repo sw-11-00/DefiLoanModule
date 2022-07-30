ab6410a8b59b558f0f1f31f0b5326647ef8183b5
Starlord0alpha <notgoatmichael@gmail.com>
2022-07-30 11:15:31 +0800

    Add approve loan message

--- vue/src/store/generated/loan/loan.loan/module/index.ts
+++ vue/src/store/generated/loan/loan.loan/module/index.ts
@@ -4,10 +4,12 @@ import { StdFee } from "@cosmjs/launchpad";
 import { SigningStargateClient } from "@cosmjs/stargate";
 import { Registry, OfflineSigner, EncodeObject, DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
 import { Api } from "./rest";
+import { MsgApproveLoan } from "./types/loan/tx";
 import { MsgRequestLoan } from "./types/loan/tx";
 
 
 const types = [
+  ["/loan.loan.MsgApproveLoan", MsgApproveLoan],
   ["/loan.loan.MsgRequestLoan", MsgRequestLoan],
   
 ];
@@ -41,6 +43,7 @@ const txClient = async (wallet: OfflineSigner, { addr: addr }: TxClientOptions =
 
   return {
     signAndBroadcast: (msgs: EncodeObject[], { fee, memo }: SignAndBroadcastOptions = {fee: defaultFee, memo: ""}) => client.signAndBroadcast(address, msgs, fee,memo),
+    msgApproveLoan: (data: MsgApproveLoan): EncodeObject => ({ typeUrl: "/loan.loan.MsgApproveLoan", value: MsgApproveLoan.fromPartial( data ) }),
     msgRequestLoan: (data: MsgRequestLoan): EncodeObject => ({ typeUrl: "/loan.loan.MsgRequestLoan", value: MsgRequestLoan.fromPartial( data ) }),
     
   };
